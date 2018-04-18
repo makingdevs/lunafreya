5acf9035534e97a7acd83b5e7013b4e2a9565700
neodevelop <juan@makingdevs.com>
2018-04-18 16:04:22 -0500

    Add init function, start genserver

--- raspi3/lib/slack/arduino.ex
+++ raspi3/lib/slack/arduino.ex
@@ -4,9 +4,14 @@ defmodule Raspi3.Arduino do
   alias Nerves.UART
 
   def start_link(opts \\ []) do
-    {:ok, pid} = UART.start_link
+    { :ok, pid } = UART.start_link(__MODULE__, :ok, opts)
     UART.open(pid, "ttyACM0", speed: 9600, active: true)
     UART.configure(pid, framing: {UART.Framing.Line, separator: "\r\n"})
+    { :ok, pid }
+  end
+
+  def init(:ok) do
+    {:ok, %{}}
   end
 end
 

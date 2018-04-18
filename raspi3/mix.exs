d444a42daac12408f197288af5492c134b430c7c
neodevelop <juan@makingdevs.com>
2018-04-18 15:51:40 -0500

    Upgrade UART version

--- raspi3/mix.exs
+++ raspi3/mix.exs
@@ -21,7 +21,7 @@ defmodule Raspi3.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nerves_uart, "~> 1.0.0"},
+      {:nerves_uart, "~> 1.2"},
       {:slack, "~> 0.12.0"}
     ]
   end

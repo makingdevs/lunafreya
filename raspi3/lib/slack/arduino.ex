9433acf5d2237084b66dc045c154b048b9528d06
neodevelop <juan@makingdevs.com>
2018-04-18 17:37:39 -0500

    Return to simple GenServer

--- raspi3/lib/slack/arduino.ex
+++ raspi3/lib/slack/arduino.ex
@@ -3,15 +3,25 @@ defmodule Raspi3.Arduino do
 
   alias Nerves.UART
 
-  def start_link(opts \\ []) do
-    { :ok, pid } = UART.start_link(__MODULE__, :ok, opts)
-    UART.open(pid, "ttyACM0", speed: 9600, active: true)
-    UART.configure(pid, framing: {UART.Framing.Line, separator: "\r\n"})
-    { :ok, pid }
+  def start_link() do
+    GenServer.start_link(__MODULE__, [])
   end
 
-  def init(:ok) do
-    {:ok, %{}}
+  def init(state) do
+    # { :ok, pid } = UART.start_link(opts)
+    # UART.open(pid, "/dev/cu.usbmodem1431", speed: 9600, active: false)
+    # UART.configure(pid, framing: {UART.Framing.Line, separator: "\r\n"})
+    # {:ok, %{}}
+    {:ok, state}
   end
+
+  def handle_call(:summary, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:put, data}, state) do
+    {:noreply, [data | state]}
+  end
+
 end
 

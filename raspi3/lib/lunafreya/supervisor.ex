1bb981a849dec330a03acb1a06adccecb854ab0a
neodevelop <juan@makingdevs.com>
2018-04-19 18:21:26 -0500

    Naming processes

--- raspi3/lib/lunafreya/supervisor.ex
+++ raspi3/lib/lunafreya/supervisor.ex
@@ -2,7 +2,7 @@ defmodule Raspi3.Supervisor do
   use Supervisor
 
   def start_link(arg) do
-    Supervisor.start_link(__MODULE__, arg)
+    Supervisor.start_link(__MODULE__, arg, name: __MODULE__)
   end
 
   def init(_arg) do

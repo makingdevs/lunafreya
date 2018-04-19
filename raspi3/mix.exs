2ea5565d88fffa5133c5b1ced25c015cdd23bce5
neodevelop <juan@makingdevs.com>
2018-04-19 13:50:36 -0500

    Add Application to mix and supervision

--- raspi3/mix.exs
+++ raspi3/mix.exs
@@ -14,6 +14,7 @@ defmodule Raspi3.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Raspi3, []},
       extra_applications: [:logger]
     ]
   end

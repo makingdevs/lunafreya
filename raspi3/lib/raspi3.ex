1fdb5d5563bdbf0b1976315e3a18a00aa139345a
neodevelop <juan@makingdevs.com>
2018-04-19 13:50:36 -0500

    Add Application to mix and supervision

--- raspi3/lib/raspi3.ex
+++ raspi3/lib/raspi3.ex
@@ -1,18 +1,10 @@
 defmodule Raspi3 do
-  @moduledoc """
-  Documentation for Raspi3.
-  """
+  use Application
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Raspi3.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def start(_type, _args) do
+    children = [
+      Supervisor.child_spec({Raspi3.Supervisor, []}, id: Raspi3.Supervisor)
+    ]
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end

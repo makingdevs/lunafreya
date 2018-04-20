6bef4caa31904b10405c1b2c4f81782ac6962aa5
neodevelop <juan@makingdevs.com>
2018-04-20 15:54:53 -0500

    Adding  as supervised process

--- raspi3/lib/lunafreya/slack_rtm.ex
+++ raspi3/lib/lunafreya/slack_rtm.ex
@@ -1,8 +1,9 @@
 defmodule Raspi3.SlackRtm do
   use Slack
+  require Logger
 
   def handle_connect(slack, state) do
-    IO.puts "Connected as #{slack.me.name}"
+    Logger.info "Connected as #{slack.me.name}"
     {:ok, state}
   end
 
@@ -13,7 +14,7 @@ defmodule Raspi3.SlackRtm do
   def handle_event(_, _, state), do: {:ok, state}
 
   def handle_info({:message, text, channel}, slack, state) do
-    IO.puts "Sending your message, captain!"
+    Logger.info "Sending your message, captain!"
 
     send_message(text, channel, slack)
 

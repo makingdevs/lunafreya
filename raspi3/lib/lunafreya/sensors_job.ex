58ec6833ac9a7120a7a1ec6f1d1850d234716903
neodevelop <juan@makingdevs.com>
2018-04-20 10:52:11 -0500

    Rename module, use UART only in Job

--- raspi3/lib/lunafreya/sensors_job.ex
+++ raspi3/lib/lunafreya/sensors_job.ex
@@ -1,30 +1,21 @@
-defmodule Raspi3.Sensors.Data do
+defmodule Raspi3.Sensors.Job do
   use GenServer
-
-  alias Nerves.UART
   require Logger
+  alias Nerves.UART
 
   def start_link(_args) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    GenServer.start_link(__MODULE__, [])
   end
 
   def init(state) do
     UART.open(Raspi3.Arduino.Serial, "/dev/cu.usbmodem1431", speed: 9600, active: false)
     UART.configure(Raspi3.Arduino.Serial, framing: {UART.Framing.Line, separator: "\r\n"})
-
     {:ok, state, 1000}
   end
 
-  def handle_call(:summary, _from, state) do
-    {:reply, state, state}
-  end
-
-  def handle_cast({:put, data}, state) do
-    {:noreply, [data | state]}
-  end
-
   def handle_info(:timeout, state) do
-    Raspi3.Writer.write_info(:os.system_time(:millisecond))
+    get_data = fn -> UART.read(Raspi3.Arduino.Serial, 1000) end
+    Raspi3.Writer.write_info(get_data)
     {:noreply, state, 1000}
   end
 

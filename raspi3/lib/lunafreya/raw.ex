5f7686df852a406270f468e3daa67b0e8ed1344e
Juan Zuñiga <neodevelop@gmail.com>
2018-04-21 04:18:59 +0000

    Add functionto handle data in sensors

--- raspi3/lib/lunafreya/raw.ex
+++ raspi3/lib/lunafreya/raw.ex
@@ -1,10 +1,25 @@
 defmodule Raspi3.Raw do
+  alias __MODULE__
 
   @enforce_keys [:time, :sensor_data]
-  defstruct time: nil, sensor_data: ""
+  defstruct time: nil, sensor_data: "", temperature: 0, light: 0, moving: 0, distance: 0
 
   def new(data) do
-    %Raspi3.Raw{time: DateTime.utc_now(), sensor_data: data}
+    %Raw{time: DateTime.utc_now(), sensor_data: data}
+    |> parse_data
+  end
+
+  def parse_data(%Raw{} = raw) do
+    sensors = String.split(raw.sensor_data, "|")
+    |> Enum.filter(fn e -> e != " " end)
+    |> Enum.map(&(String.trim/1))
+    |> Enum.map(&(Integer.parse/1))
+    [{temperature, _}, {light, _}, {moving, _}, {distance, _}] = sensors
+    %Raspi3.Raw{raw | temperature: temperature, light: light, moving: moving, distance: distance}
+  end
+
+  def summary(%Raw{} = raw) do
+    "Temperature: #{raw.temperature}, Light: #{raw.light}, change position?: #{raw.moving}, Distance: #{raw.distance}"
   end
 
 end

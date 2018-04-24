bce956aa46c0cc890825ea0d58e049400c739b1a
neodevelop <neodevelop@gmail.com>
2018-04-24 05:02:51 -0500

    Add a Supervisor

--- raspi3/lib/lunafreya/supervisor.ex
+++ raspi3/lib/lunafreya/supervisor.ex
@@ -8,13 +8,11 @@ defmodule Raspi3.Supervisor do
   def init(_arg) do
 
     token = Application.get_env(:slack, :api_token)
-    camera = Application.get_env(:picam, :camera, Picam.Camera)
 
     children = [
-      {Nerves.UART, [name: Raspi3.Arduino.Serial]},
+      Raspi3.HardwareSupervisor,
       # {Raspi3.Sensors.Job, []},
       {Raspi3.Writer, []},
-      camera,
       %{
         id: Slack.Bot,
         name: Raspi3.Slack,

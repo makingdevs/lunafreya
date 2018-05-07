ec234dde0ba5a507c59dd2c17529bcb03c25277a
neodevelop <juan@makingdevs.com>
2018-05-07 16:59:17 -0500

    Setup a GenServer for change the values in data raw

--- raspi3/lib/lunafreya/uart_fake.ex
+++ raspi3/lib/lunafreya/uart_fake.ex
@@ -10,11 +10,20 @@ defmodule Raspi3.UART.Fake do
   end
 
   def start_link(_args) do
-    GenServer.start_link(__MODULE__, [])
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def init(state) do
+  def init(_) do
+    state = " | 1 | 1 | 1 | 1 | "
     {:ok, state}
+  end
+
+  def write_sensors_values(data) do
+    GenServer.cast(__MODULE__, {:write, data})
+  end
+
+  def read_sensors_values() do
+    GenServer.call(__MODULE__, {:read})
   end
 
   def open(module_name, port, opts) do
@@ -29,7 +38,16 @@ defmodule Raspi3.UART.Fake do
 
   def read(_module_name, _opts) do
     # Logger.info "Reading value from Serial port"
-    {:ok,  " | 1 | 1 | 1 | 1 | "}
+    data = read_sensors_values()
+    {:ok,  data}
+  end
+
+  def handle_call({:read}, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:write, data}, _state) do
+    {:noreply, data}
   end
 
 end

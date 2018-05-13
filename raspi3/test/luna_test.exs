b0268ae62dd7b3a5bf642778a9be713ded9bae19
neodevelop <neodevelop@gmail.com>
2018-05-12 23:07:30 -0500

    Thinking in tests

--- raspi3/test/luna_test.exs
+++ raspi3/test/luna_test.exs
@@ -3,21 +3,28 @@ defmodule Raspi3.Luna.EyesTest do
 
   alias Raspi3.Luna.Eyes
 
-  test "object is in range of vision" do
-    assert Eyes.object_is_in_range(100, 100) == :in_range
+  test "is seeing the same object" do
+    assert Eyes.see_the_same_object(100, 100) == :the_same
   end
 
-  test "object is not in range of vision" do
-    assert Eyes.object_is_in_range(400, 100) == :out_of_range
+  test "is not seeing the same object" do
+    assert Eyes.see_the_same_object(400, 100) == :not_the_same
+    assert Eyes.see_the_same_object(400, 10) == :not_the_same
   end
 
   test "luna have to use the eyes" do
-    assert Eyes.have_to_see(:not_busy) == :watch
+    assert Eyes.have_to_see(:not_busy) == :watching
   end
 
   test "luna have not to use the eyes" do
-    assert Eyes.have_to_see(:busy) == :not_watch
+    assert Eyes.have_to_see(:busy) == :not_watching
+  end
+
+  test "luna has to record the image" do
+    assert Eyes.preserve_the_moment( {100,100}, :busy    ) == :dont_record
+    assert Eyes.preserve_the_moment( {100,100}, :not_busy) == :dont_record
+    assert Eyes.preserve_the_moment( {400,100}, :busy    ) == :record_image
+    assert Eyes.preserve_the_moment( {400,100}, :not_busy) == :dont_record
   end
 
 end
-

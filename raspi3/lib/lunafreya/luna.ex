422020c0bf486d6e66eb4edd6c0d3f07a1ae345a
neodevelop <neodevelop@gmail.com>
2018-05-13 00:07:34 -0500

    Start again, now with eyes!

--- raspi3/lib/lunafreya/luna.ex
+++ raspi3/lib/lunafreya/luna.ex
@@ -10,45 +10,24 @@ defmodule Raspi3.Luna do
   end
 
   def init(_) do
-    {:ok, [eyes_watching: false]}
+    {:ok, {:dont_record}}
   end
 
   def think(%Raw{} = data, data_for_last_seconds) do
     GenServer.cast(__MODULE__, {:think, data, data_for_last_seconds})
   end
 
-  def handle_cast({:think, %Raw{} = data, data_for_last_seconds}, [eyes_watching: eyes_watching] = _state) do
-    continue_watching = case eyes_watching do
-      true ->
-        IO.puts "Eyes busy"
-      false ->
-        IO.puts "Let's watch"
-        stats = Raw.statistics(data_for_last_seconds)
-        case stats["distance"][:median] - 50 <= data.distance || data.distance >= stats["distance"][:median] + 50  do
-          true ->
-            IO.puts "Take the picture"
-            IO.puts "#{stats["distance"][:median] - 50} <= #{data.distance} || #{data.distance} >= #{stats["distance"][:median] + 50}"
-            take_the_picture()
-          false ->
-            IO.puts "NO PIC"
-            IO.puts "#{stats["distance"][:median] - 50} <= #{data.distance} || #{data.distance} >= #{stats["distance"][:median] + 50}"
-        end
-    end
-    # TODO: Compare the values
-    # Take the picture and block the camera, maybe a message
-    # Block the camera while picture
-    # Unblock after the picture
-
-    {:noreply, [eyes_watching: true]}
+  def handle_cast({:think, %Raw{} = data, data_for_last_seconds}, state) do
+    {:noreply, state}
   end
 
-	def handle_info({:picture_taked}, _state) do
-		{:noreply, [eyes_watching: false]}
-	end
+  def handle_info({:picture_taked}, state) do
+    {:noreply, state}
+  end
 
-	def take_the_picture() do
-		Process.send_after(self(), {:picture_taked}, 3 *  1000)
-	end
+  def take_the_picture() do
+    Process.send_after(self(), {:picture_taked}, 3 *  1000)
+  end
 
   def see_what_happens() do
     Picam.set_size(640, 0)

a13a28f10fa4d7b51a821dd1a16fc70916a1be5e
neodevelop <neodevelop@gmail.com>
2018-05-12 23:11:55 -0500

    Solve tests, one pending...

--- raspi3/lib/lunafreya/luna_eyes.ex
+++ raspi3/lib/lunafreya/luna_eyes.ex
@@ -2,18 +2,22 @@ defmodule Raspi3.Luna.Eyes do
 
   @distance_for_diff 50
 
-  def object_is_in_range(distance, median) do
+  def see_the_same_object(distance, median) do
     case distance <= (median + @distance_for_diff) && distance >= (median - @distance_for_diff) do
-      true -> :in_range
-      false -> :out_of_range
+      true -> :the_same
+      false -> :not_the_same
     end
   end
 
   def have_to_see(eyes_state) do
     case eyes_state do
-      :busy -> :not_watch
-      :not_busy -> :watch
+      :busy -> :not_watching
+      :not_busy -> :watching
     end
+  end
+
+  def preserve_the_moment({distance, median}, eyes_watching) do
+    :dont_record
   end
 
 end

047c7b1a93a7ab241bac88c4539432b39ed75d2e
neodevelop <juan@makingdevs.com>
2018-05-15 15:35:25 -0500

    Validations for hw and data.
    - The data with distance > 0 should be filtered
    - For some reason, arduino lost connection or could not get data
      The function `get_data` cause an error

--- raspi3/lib/lunafreya/sensor_data.ex
+++ raspi3/lib/lunafreya/sensor_data.ex
@@ -19,11 +19,21 @@ defmodule Raspi3.SensorData do
   end
 
   def handle_cast({:write, get_data}, state) do
-    {:ok, data} = get_data.()
-    raw = Raw.new(data)
-    data_for_last_seconds = state |> Enum.take(30)
-    Raspi3.Luna.think(raw, data_for_last_seconds)
-    {:noreply, [raw | state]}
+    case {result, data} = get_data.() do
+      {:ok, data} ->
+        raw = Raw.new(data)
+        data_for_last_seconds =
+          state
+          |> Enum.filter(fn(%Raw{distance: distance}) -> distance > 0  end)
+          |> Enum.take(30)
+        Raspi3.Luna.think(raw, data_for_last_seconds)
+        {:noreply, [raw | state]}
+
+      {:error, _} ->
+        IO.puts "Error getting data"
+        {:noreply, state}
+
+    end
   end
 
   def handle_call({:summary}, _from, state) do

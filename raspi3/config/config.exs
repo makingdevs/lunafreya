e9a681c309116d4c444a9a8652eb6845a11ea167
Jose Juan Reyes <neodevelop@gmail.com>
2020-08-31 18:14:46 -0500

    Update config for Nerves

--- raspi3/config/config.exs
+++ raspi3/config/config.exs
@@ -1,18 +1,32 @@
-use Mix.Config
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+import Config
 
-# You can configure your application as:
-#
-#     config :raspi3, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:raspi3, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+config :pi3, target: Mix.target()
+
+# Customize non-Elixir parts of the firmware. See
+# https://hexdocs.pm/nerves/advanced-configuration.html for details.
+
+config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+
+# Set the SOURCE_DATE_EPOCH date for reproducible builds.
+# See https://reproducible-builds.org/docs/source-date-epoch/ for more information
+
+config :nerves, source_date_epoch: "1598563026"
+
+# Use Ringlogger as the logger backend and remove :console.
+# See https://hexdocs.pm/ring_logger/readme.html for more information on
+# configuring ring_logger.
+
+config :logger, backends: [RingLogger]
+
+if Mix.target() != :host do
+  import_config "target.exs"
+end
 
 config :slack, api_token: System.get_env("SLACK_TOKEN")
 
-import_config "#{Mix.env}.exs"
+# import_config "#{Mix.env}.exs"

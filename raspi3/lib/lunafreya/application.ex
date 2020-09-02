e6c38ac9e836d51a9e4a67b273b4bfdb2c489c3f
Jose Juan Reyes <neodevelop@gmail.com>
2020-09-01 19:05:24 -0500

    Move children to be supervised

--- raspi3/lib/lunafreya/application.ex
+++ raspi3/lib/lunafreya/application.ex
@@ -1,40 +1,27 @@
 defmodule Pi3.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Pi3.Supervisor]
 
     children =
       [
-        # Children for all targets
-        # Starts a worker by calling: Pi3.Worker.start_link(arg)
-        # {Pi3.Worker, arg},
+        Supervisor.child_spec({Raspi3.Supervisor, []}, id: Raspi3.Supervisor),
+        Raspi3.Luna,
+        Raspi3.Luna.EyesServer
       ] ++ children(target())
 
     Supervisor.start_link(children, opts)
   end
 
-  # List all child processes to be supervised
   def children(:host) do
-    [
-      # Children that only run on the host
-      # Starts a worker by calling: Pi3.Worker.start_link(arg)
-      # {Pi3.Worker, arg},
-    ]
+    []
   end
 
   def children(_target) do
-    [
-      # Children for all targets except host
-      # Starts a worker by calling: Pi3.Worker.start_link(arg)
-      # {Pi3.Worker, arg},
-    ]
+    []
   end
 
   def target() do

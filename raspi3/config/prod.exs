b7c1f3a26ce2b7661a3d9fe2da4df57338598e48
Jose Juan Reyes <neodevelop@gmail.com>
2020-09-08 16:52:13 -0500

    Suse , the tuple is runtime

--- raspi3/config/prod.exs
+++ raspi3/config/prod.exs
@@ -1,14 +1,14 @@
 use Mix.Config
 
-config :pi3, uploader: Raspi3.S3
 config :pi3, uart: Nerves.UART
 
+config :pi3, uploader: Raspi3.S3
+
 config :arc,
-  # or Arc.Storage.Local
   storage: Arc.Storage.S3,
-  # if using Amazon S3
-  bucket: {:system, "AWS_S3_BUCKET"}
+  bucket: System.get_env("AWS_S3_BUCKET"),
+  storage_dir: "luna_freya"
 
 config :ex_aws,
-  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
-  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY")
